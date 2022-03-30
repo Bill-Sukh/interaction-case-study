2735dcd368f87792ddfc8ed6caa4548b5abba279
bill sukh <bill.sukh97@gmail.com>
2022-03-29 19:49:03 -0500

    final

--- js/gallery-thumbnail.js
+++
@@ -1,6 +0,0 @@
-class Image {
-    constructor(fileName, width) {
-      this.height = height;
-      this.width = width;
-    }
-  }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const gallery_content = document.querySelector('.gallery__content');
+const imgElement = document.querySelector('#js-editor__img');
 
 function initHandlers() {
     initDragDropHandlers();
@@ -16,6 +17,7 @@ function initDragDropHandlers() {
     dropHandle.addEventListener("dragover", dragover, false);
     dropHandle.addEventListener("drop", drop, false);
     dropHandle.addEventListener("dragleave", dragleave, false);
+
 }
 
 function dragenter(e) {
@@ -44,18 +46,33 @@ function dragleave(e){
     e.preventDefault();
 
     gallery_content.style.borderColor = 'rgba(130, 134, 136, 0.2)';
-    gallery_content.style.overflowX = 'scroll'; 
-    gallery_content.style.overflowY = 'hidden';
+}
+
+function colorSelected(parentElement, selectedIndex, className){
+    console.log('selectedIndex: ' + selectedIndex);
+
+    if(parentElement && selectedIndex >= 0){
+        console.log(parentElement.children[selectedIndex]);
+        parentElement.children[selectedIndex].classList.add(className);
+        for(let i = 0; i < parentElement.children.length; i++){
+            if(i != selectedIndex){
+                if(parentElement.children[i].classList.contains(className)){
+                    parentElement.children[i].classList.remove(className);
+                    console.log('removed index' + selectedIndex);
+                }
+            }
+        }
+    }
 }
 
 function handleFiles(files) { //images[]
     const fileName = document.getElementById('js-metadata__name')
     const mimeTypeText = document.getElementById('js-metadata__mime');
     const fileSizeText = document.getElementById('js-metadata__size');
+    const className = 'selected-thumb';
 
     for (let i = 0; i < files.length; i++) {
         const file = files[i];
-        // console.log(typeof (files[i].size));
         const placeholderText = document.getElementById('js-gallery__placeholder'); // Uses the js- prefix as a handle
         placeholderText.style.display= "none";
 
@@ -67,13 +84,14 @@ function handleFiles(files) { //images[]
             const reader = new FileReader();
             reader.onload = (e) => { 
                 document.getElementById('js-editor__img').src = e.target.result; 
-                
                 fileName.innerHTML = file.name;
                 mimeTypeText.innerHTML = file.type;
                 fileSizeText.innerHTML = humanizeFileSize(file.size);
                 const placeholderText = document.getElementById('js-editor__placeholder'); // Uses the js- prefix as a handle
                 placeholderText.style.display= "none";
+                colorSelected(img.parentElement, i+1, className);    
             };
+            
             reader.readAsDataURL(file);
         })
         const galleryContentElement = document.getElementById('js-gallery__content');
@@ -94,16 +112,70 @@ function handleFiles(files) { //images[]
  * They are called by actions performed on the sidebar 
  * */
 function initFormHandlers() {
-    document.forms.namedItem('clip').addEventListener('change', (e) => changeShape(e.target.value));
-    document.forms.namedItem('filter').addEventListener('change', (e) => changeFilter(e.target.value));
+    document.getElementById('clip').addEventListener('click', (e) => changeShape(e.target));
+    document.getElementById('filter').addEventListener('click', (e) => changeFilter(e.target));
 }
 
-function changeShape(shape) {
-    // TODO: Fill me in
+function hasPrefix(className, classList){
+    for(let i = 0; i < classList.length; i++){
+        if(classList[i].toString().includes(className)){
+            return true;
+        }
+    }
+
+    return false;
 }
 
-function changeFilter(filter) {
-    // TODO: Fill me in
+function changeShape(clipChoice) {
+    const parentElementArr = clipChoice.parentElement.children;
+    const parentElement = clipChoice.parentElement;
+
+    if(imgElement.hasAttribute('class')){
+        if(hasPrefix('clip', imgElement.classList)){
+            for(let i = 0; i < parentElementArr.length; i++){
+                if(imgElement.classList.contains(parentElementArr[i].dataset.clip)){
+                    imgElement.classList.remove(parentElementArr[i].dataset.clip);
+                    imgElement.classList.add(clipChoice.dataset.clip);
+                }
+            }
+        }
+        else{
+            imgElement.classList.add(clipChoice.dataset.clip);
+        }
+    }
+    else{
+        console.log('initial clip');
+        imgElement.classList.add(clipChoice.dataset.clip);
+    }
+    
+    colorSelected(parentElement, [].indexOf.call(parentElementArr, clipChoice),'selected');
+    // console.log([].indexOf.call(parentElementArr, clipChoice));
+}
+
+function changeFilter(filterChoice) {
+    const parentElementArr = filterChoice.parentElement.children;
+    const parentElement = filterChoice.parentElement;
+    console.log(filterChoice.dataset.filter);
+    
+    if(imgElement.hasAttribute('class')){    
+        if(hasPrefix('filter', imgElement.classList)){
+            for(let i = 0; i < parentElementArr.length; i++){
+                if(imgElement.classList.contains(parentElementArr[i].dataset.filter)){
+                    imgElement.classList.remove(parentElementArr[i].dataset.filter);
+                    imgElement.classList.add(filterChoice.dataset.filter);
+                }
+            }
+        }
+        else{
+            imgElement.classList.add(filterChoice.dataset.filter);
+        }
+    }
+    else{
+        console.log('initial filter');
+        imgElement.classList.add(filterChoice.dataset.filter);
+    }   
+
+    colorSelected(parentElement, [].indexOf.call(parentElementArr, filterChoice),'selected');
 }
 
 // Use this function to display filesize in a human-readable format
